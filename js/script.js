17b2ce2f889bbaf28b6feec0f8a7b1432adfbf03
kastastin <kastastin.main@gmail.com>
2024-10-18 18:22:01 +0300

    add adaptive (>320px)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menuMobile = document.querySelector(".menu-mobile");
+
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu_open");
+});
+
 const swiper = new Swiper(".swiper", {
   loop: true,
-  slidesPerView: 2,
-  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
   navigation: {
     nextEl: ".swiper-button-left",
     prevEl: ".swiper-button-right",
